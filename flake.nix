01fc1afe1c56ec5a8b937b2bc3d95d6988db49ab
Ethan Anthony <ethan.anthony@du.edu>
2025-05-09 17:22:54 -0600

    updated to use flake-utils for multi-system support

--- flake.nix
+++ flake.nix
@@ -2,24 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.aarch64-linux = {
-      default = (inputs.nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-linux;
-        modules = [
-          ./default.nix
-        ];
+  outputs = { nixpkgs, flake-utils, nvf, ... }:
+  let
+    systems = [ "aarch64-linux" "x86_64-linux" ];
+  in
+    flake-utils.lib.eachSystem systems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      neovim = (nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [ ./default.nix ];
       }).neovim;
-    };
-    packages.x86_64-linux = {
-      default = (inputs.nvf.lib.neovimConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ./default.nix
-        ];
-      }).neovim;
-    };
-  };
+    in {
+      defaultPackage = neovim;
+      packages.default = neovim;
+    }
+  );
 }
