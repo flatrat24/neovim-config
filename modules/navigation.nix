1e6d9bf51c491be597d0c745bb84288b092dc3ff
Ethan Anthony <ethananthony@worldofmail.com>
2025-08-15 21:57:35 -0600

    stripped eyeliner from navigation

--- modules/navigation.nix
+++ modules/navigation.nix
@@ -4,43 +4,32 @@ let
 in
 {
   imports = [ ];
+
   options = {
     navigation = {
-      enable = lib.mkEnableOption "enables navigation options";
-      leap.enable = lib.mkOption {
+      enable = lib.mkOption {
         type = lib.types.bool;
-        default = cfg.enable;
+        default = true;
+        description = "enables navigation";
       };
-      eyeliner = lib.mkOption {
+      leap.enable = lib.mkOption {
         type = lib.types.bool;
         default = cfg.enable;
       };
     };
   };
+
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
       vim = {
         keymaps = [
-          { key = ",";     action = ";"; silent = true; mode = [ "n" "x" "o" ]; desc = "Repeat last find motion"; }
+          # { key = ",";     action = ";"; silent = true; mode = [ "n" "x" "o" ]; desc = "Repeat last find motion"; }
           { key = "<tab>"; action = "%"; silent = true; mode = [ "n" "x" "o" ]; desc = "Move to twin bracket";    }
           { key = "H";     action = "0"; silent = true; mode = [ "n" "x" "o" ]; desc = "Move to start of line";   }
           { key = "L";     action = "$"; silent = true; mode = [ "n" "x" "o" ]; desc = "Move to end of line";     }
         ];
       };
     }
-    (lib.mkIf cfg.eyeliner.enable {
-      vim.extraPlugins = {
-        eyeliner-nvim = {
-          package = pkgs.vimPlugins.eyeliner-nvim;
-          setup = ''
-            require('eyeliner').setup({
-              highlight_on_key = true;
-              dim = true;
-            })
-          '';
-        };
-      };
-    })
     (lib.mkIf cfg.leap.enable {
       vim.extraPlugins = {
         leap-nvim = {
