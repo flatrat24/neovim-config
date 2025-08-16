934fa4c64115d35bca6e2ab3d60d6763ae4c61dc
Ethan Anthony <ethananthony@worldofmail.com>
2025-08-15 21:58:24 -0600

    imported new modules + treesitter

--- basic.nix
+++ basic.nix
@@ -1,12 +1,36 @@
-{ ... }: {
+{ pkgs, lib, ... }: {
   imports = [
     ./modules/navigation.nix
     ./modules/colorscheme.nix
+    ./modules/filesystem.nix
+    ./modules/folding.nix
+    ./modules/spellcheck.nix
+    ./modules/visuals.nix
   ];
 
   options = { };
 
   config = {
-    # colorscheme.enable = true;
+    vim = {
+      treesitter = {
+        enable = true;
+        grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+      };
+      options = {
+        expandtab = true;
+        number = true;
+        numberwidth = 1;
+        tabstop = 2;
+        softtabstop = 2;
+        shiftwidth = 2;
+        wrap = false;
+        scrolloff = 10;
+        sidescrolloff = 5;
+        ignorecase = true;
+        smartcase = true;
+        cursorline = true;
+        formatoptions = "jql";
+      };
+    };
   };
 }
